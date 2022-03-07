f4f67865a33372269ca483ebdd7028d1a5214b9c
Amakris12 <amakri386@west-mec.org>
2022-03-07 15:20:10 -0700

    Started Celtics Project

---
+++ jays/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaBasketballBall } from "react-icons/fa";
+
+const header = () => {
+    return (
+        <div className="head">
+            <h2 className='title'>The Boston Celtics{FaBasketballBall}</h2>
+        </div>
+    )
+}
+
+export default header
---
+++ jays/src/Pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../Components/Header'
+const home = () => {
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default home
